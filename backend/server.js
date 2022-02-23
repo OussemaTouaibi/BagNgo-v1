9516ce0995e43bbecdccb7159a631b80d0948a32
OussemaTouaibi <oussema.touaibi@esprit.tn>
2022-02-23 11:32:41 -1200

    installing nd setting up Nodemon

--- backend/server.js
+++ backend/server.js
@@ -6,5 +6,5 @@ const dotenv = require('dotenv');
 dotenv.config({ path: 'backend/config/config.env'});
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server Started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode. `)
+    console.log(`Server Started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode. `);
 });
