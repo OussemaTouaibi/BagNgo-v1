1d3cea5cd5a535b77de9f2685c0ee42d1edfa678
OussemaTouaibi <oussema.touaibi@esprit.tn>
2022-02-23 15:41:19 -1200

    Backend Error Handling

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+const errorMiddleware = require('./middleware/errors');
+
 app.use(express.json());
+
+
+// Middleware to handle eroors
+app.use(errorMiddleware);
 
 module.exports = app;
