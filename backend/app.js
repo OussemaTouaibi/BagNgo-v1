e41c79e8006c9724ab07e11b9830f36acfa614b0
OussemaTouaibi <oussema.touaibi@esprit.tn>
2022-02-28 15:12:17 -1200

    Backend Authentication & Authorization

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+
 const errorMiddleware = require('./middleware/errors');
 
 app.use(express.json());
+app.use(cookieParser());
+
+
+// Import all routes
+const auth = require('./routes/auth');
+
+
+app.use('/api/v1', auth);
 
 
 // Middleware to handle eroors
---
+++ backend/models/user.js
@@ -0,0 +1,366 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: [true, 'Please enter your first name'],
+        maxLength: [15, 'Your name cannot exceed 15 characters']
+    },
+    lname: {
+        type: String,
+        required: [true, 'Please enter your last name'],
+        maxLength: [15, 'Your name cannot exceed 15 characters']
+    },
+    birthday: {
+        type: Date,
+        required: [true, 'Please enter your birthday date'],
+        
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter your email'],
+        unique: true,
+        validate: [validator.isEmail, 'Please enter valid email address']
+    },
+    country: {
+        type: String,
+        required: [true, 'Please select your country'],
+        enum: {
+            values: [
+                "Afghanistan",
+	"Albania",
+	"Algeria",
+	"American Samoa",
+	"Andorra",
+	"Angola",
+	"Anguilla",
+	"Antarctica",
+	"Antigua and Barbuda",
+	"Argentina",
+	"Armenia",
+	"Aruba",
+	"Australia",
+	"Austria",
+	"Azerbaijan",
+	"Bahamas (the)",
+    "Bahrain",
+	"Bangladesh",
+	"Barbados",
+	"Belarus",
+	"Belgium",
+	"Belize",
+	"Benin",
+	"Bermuda",
+	"Bhutan",
+	"Bolivia (Plurinational State of)",
+	"Bonaire, Sint Eustatius and Saba",
+	"Bosnia and Herzegovina",
+	"Botswana",
+	"Bouvet Island",
+	"Brazil",
+	"British Indian Ocean Territory (the)",
+	"Brunei Darussalam",
+	"Bulgaria",
+	"Burkina Faso",
+	"Burundi",
+	"Cabo Verde",
+	"Cambodia",
+	"Cameroon",
+	"Canada",
+	"Cayman Islands (the)",
+	"Central African Republic (the)",
+	"Chad",
+	"Chile",
+	"China",
+	"Christmas Island",
+	"Cocos (Keeling) Islands (the)",
+	"Colombia",
+	"Comoros (the)",
+	"Congo (the Democratic Republic of the)",
+	"Congo (the)",
+	"Cook Islands (the)",
+	"Costa Rica",
+	"Croatia",
+	"Cuba",
+	"Curaçao",
+	"Cyprus",
+	"Czechia",
+	"Côte d'Ivoire",
+	"Denmark",
+	"Djibouti",
+	"Dominica",
+	"Dominican Republic (the)",
+	"Ecuador",
+	"Egypt",
+	"El Salvador",
+	"Equatorial Guinea",
+	"Eritrea",
+	"Estonia",
+	"Eswatini",
+	"Ethiopia",
+	"Falkland Islands (the) [Malvinas]",
+	"Faroe Islands (the)",
+	"Fiji",
+	"Finland",
+	"France",
+	"French Guiana",
+	"French Polynesia",
+	"French Southern Territories (the)",
+	"Gabon",
+	"Gambia (the)",
+	"Georgia",
+	"Germany",
+	"Ghana",
+	"Gibraltar",
+	"Greece",
+	"Greenland",
+	"Grenada",
+	"Guadeloupe",
+	"Guam",
+	"Guatemala",
+	"Guernsey",
+	"Guinea",
+	"Guinea-Bissau",
+	"Guyana",
+	"Haiti",
+	"Heard Island and McDonald Islands",
+	"Holy See (the)",
+	"Honduras",
+	"Hong Kong",
+	"Hungary",
+	"Iceland",
+	"India",
+	"Indonesia",
+	"Iran (Islamic Republic of)",
+	"Iraq",
+	"Ireland",
+	"Isle of Man",
+	"Israel",
+	"Italy",
+	"Jamaica",
+	"Japan",
+	"Jersey",
+	"Jordan",
+	"Kazakhstan",
+	"Kenya",
+	"Kiribati",
+	"Korea (the Democratic People's Republic of)",
+	"Korea (the Republic of)",
+	"Kuwait",
+	"Kyrgyzstan",
+	"Lao People's Democratic Republic (the)",
+	"Latvia",
+	"Lebanon",
+	"Lesotho",
+	"Liberia",
+	"Libya",
+	"Liechtenstein",
+	"Lithuania",
+	"Luxembourg",
+	"Macao",
+	"Madagascar",
+	"Malawi",
+	"Malaysia",
+	"Maldives",
+	"Mali",
+	"Malta",
+	"Marshall Islands (the)",
+	"Martinique",
+	"Mauritania",
+	"Mauritius",
+	"Mayotte",
+	"Mexico",
+	"Micronesia (Federated States of)",
+	"Moldova (the Republic of)",
+	"Monaco",
+	"Mongolia",
+	"Montenegro",
+	"Montserrat",
+	"Morocco",
+	"Mozambique",
+	"Myanmar",
+	"Namibia",
+	"Nauru",
+	"Nepal",
+	"Netherlands (the)",
+	"New Caledonia",
+	"New Zealand",
+	"Nicaragua",
+	"Niger (the)",
+	"Nigeria",
+	"Niue",
+	"Norfolk Island",
+	"Northern Mariana Islands (the)",
+	"Norway",
+	"Oman",
+	"Pakistan",
+	"Palau",
+	"Palestine, State of",
+	"Panama",
+	"Papua New Guinea",
+	"Paraguay",
+	"Peru",
+	"Philippines (the)",
+	"Pitcairn",
+	"Poland",
+	"Portugal",
+	"Puerto Rico",
+	"Qatar",
+	"Republic of North Macedonia",
+	"Romania",
+	"Russian Federation (the)",
+	"Rwanda",
+	"Réunion",
+	"Saint Barthélemy",
+	"Saint Helena, Ascension and Tristan da Cunha",
+	"Saint Kitts and Nevis",
+	"Saint Lucia",
+	"Saint Martin (French part)",
+	"Saint Pierre and Miquelon",
+	"Saint Vincent and the Grenadines",
+	"Samoa",
+	"San Marino",
+	"Sao Tome and Principe",
+	"Saudi Arabia",
+	"Senegal",
+	"Serbia",
+	"Seychelles",
+	"Sierra Leone",
+	"Singapore",
+	"Sint Maarten (Dutch part)",
+	"Slovakia",
+	"Slovenia",
+	"Solomon Islands",
+	"Somalia",
+	"South Africa",
+	"South Georgia and the South Sandwich Islands",
+	"South Sudan",
+	"Spain",
+	"Sri Lanka",
+	"Sudan (the)",
+	"Suriname",
+	"Svalbard and Jan Mayen",
+	"Sweden",
+	"Switzerland",
+	"Syrian Arab Republic",
+	"Taiwan",
+	"Tajikistan",
+	"Tanzania, United Republic of",
+	"Thailand",
+	"Timor-Leste",
+	"Togo",
+	"Tokelau",
+	"Tonga",
+	"Trinidad and Tobago",
+	"Tunisia",
+	"Turkey",
+	"Turkmenistan",
+	"Turks and Caicos Islands (the)",
+	"Tuvalu",
+	"Uganda",
+	"Ukraine",
+	"United Arab Emirates (the)",
+	"United Kingdom of Great Britain and Northern Ireland (the)",
+	"United States Minor Outlying Islands (the)",
+	"United States of America (the)",
+	"Uruguay",
+	"Uzbekistan",
+	"Vanuatu",
+	"Venezuela (Bolivarian Republic of)",
+	"Viet Nam",
+	"Virgin Islands (British)",
+	"Virgin Islands (U.S.)",
+	"Wallis and Futuna",
+	"Western Sahara",
+	"Yemen",
+	"Zambia",
+	"Zimbabwe",
+	"Åland Islands"
+            ],
+        }
+    },
+    phoneNum: {
+        type: String,
+        required: [true, 'Please enter your phone number'],
+        unique: true,
+        validate: [validator.isMobilePhone, 'Please enter valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        minLength: [6, 'Your password must be longer than 6 characters'],
+        select: false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+    },
+        url: {
+            type: String,
+            required: true
+        },
+    },
+   
+    role:  {
+        type: String,
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date
+
+})
+
+
+    // Encrypting password before saving user
+    userSchema.pre('save', async function (next) {
+        if(!this.isModified('password')) {
+            next()
+        }
+        this.password = await bcrypt.hash(this.password, 10)
+    })
+
+    // Compare user password
+    userSchema.methods.comparePassword = async function (enteredPassword) {
+        return await bcrypt.compare(enteredPassword, this.password)
+    }
+
+    // Return JWT token
+    userSchema.methods.getJwtToken = function () {
+        return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_TIME
+        })
+
+    }
+
+    // Generate password reset token
+    userSchema.methods.getResetPasswordToken = function () {
+       
+       
+        // Generate token
+        const resetToken = crypto.randomBytes(20).toString('hex');
+        
+
+        // Hash and set to resetPasswordToken
+        this.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+    
+    
+        // Set token expire time
+        this.resetPasswordExpire = Date.now() + 30 * 60 * 1000
+
+        return resetToken
+    }
+
+
+module.exports = mongoose.model('User', userSchema);
+
+
+	
